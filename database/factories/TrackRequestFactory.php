d713d0c8f35a4a8563fe63ab75e744decf37afdb
ifeanyi-ajimah <ajimahifeanyi@gmail.com>
2022-05-04 20:49:17 +0100

    track factory

--- database/factories/TrackRequestFactory.php
+++ database/factories/TrackRequestFactory.php
@@ -20,7 +20,13 @@ class TrackRequestFactory extends Factory
             },
             'status' => 1,
             'user_id' => 1,
-            'request_data' => 'admin',
+            'request_data' => [
+                'id'         => 1,
+                'first_name' => $this->faker->name,
+                'last_name'  => $this->faker->name,
+                'email'      => $this->faker->name,
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                ],
             'creator_id' => 1,
         ];
     }
